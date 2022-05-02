44d91ffc29e5a7c6fb0432983e56d2a5b169772a
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-05-02 11:17:55 -0400

    Cards section, Bootstrap-icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Cards from "./components/Cards";
 import HeroSection from "./components/HeroSection";
 
 import Navbar from "./components/Navbar";
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <HeroSection />
       <Newsletter />
+      <Cards />
     </div>
   );
 }
---
+++ src/components/Cards.js
@@ -0,0 +1,66 @@
+function Cards() {
+  return (
+    <section
+      className="p-5
+  "
+    >
+      <div className="container">
+        <div className="row text-center">
+          <div className="col-md">
+            <div className="card bg-dark text-light">
+              <div className="card-body text-center">
+                <div className="h1 mb-3">
+                  <i className="bi bi-laptop" />
+                </div>
+                <h3 className="card-title mp-3">Virtual</h3>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                  deserunt rem ea unde necessitatibus velit.
+                </p>
+                <a href="/virtual" className="btn btn-primary ">
+                  Read More
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md">
+            <div className="card bg-secondary text-light">
+              <div className="card-body text-center">
+                <div className="h1 mb-3">
+                  <i className="bi bi-person-square" />
+                </div>
+                <h3 className="card-title mp-3">Hybrid</h3>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                  deserunt rem ea unde necessitatibus velit.
+                </p>
+                <a href="/hybrid" className="btn btn-dark ">
+                  Read More
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md">
+            <div className="card bg-dark text-light">
+              <div className="card-body text-center">
+                <div className="h1 mb-3">
+                  <i className="bi bi-people" />
+                </div>
+                <h3 className="card-title mp-3">In Person</h3>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                  deserunt rem ea unde necessitatibus velit.
+                </p>
+                <a href="/virtual" className="btn btn-primary ">
+                  Read More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Cards;
